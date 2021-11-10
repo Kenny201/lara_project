40ef366db17aba19c9aa5401c3f8e5bea40edda7
Vitalik <panins899@gmail.com>
2021-11-10 13:34:54 +0300

    feature: modified HomeController

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,11 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Routing\Controller as BaseController;
 
 class HomeController extends BaseController
 {
     public function index(){
-        return view('home.index');
+        $posts = Post::with(['user', 'category'])->get();
+
+        return view('home.index', ['posts' => $posts]);
     }
 }
